4715acb712c813421215759e09a0a08d5885067e
Ronen Zolicha <ronen1245@gmail.com>
2019-04-16 14:03:47 +0300

    Base login implementation

--- App.py
+++ App.py
@@ -1,9 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
 @app.route('/')
-def hello_world():
-    return "Hello World"
+def index():
+    return render_template('index.html')
 
-app.run(port=5000)
+@app.route('/login/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
